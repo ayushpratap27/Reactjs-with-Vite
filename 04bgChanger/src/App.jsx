f1638503918c3269597a84eca3c1313813db8646
Ayush Pratap <122193870+ayushpratap27@users.noreply.github.com>
2024-10-26 22:38:37 +0530

    Update App.jsx

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -6,12 +6,14 @@ function App() {
 
   return (
     <div className="w-full h-screen duration-200"
-    style={{backgroundColor: color}}
+    style={{backgroundColor: color}} // Here we have to use two curly braces as per the syntax
     >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
-          onClick={() => setColor("red")}
+          onClick={() => setColor("red")}  // onClick demands function inside it, if we pass setColor so it's okay
+          // but then we are not able to pass parameters and if we pass setColor("red") it is going to give the 
+          // return value of function, that's why we are firing the callback inside it
           className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
           style={{backgroundColor: "red"}}
           >Red</button>
