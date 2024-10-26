78aedf9076493ab45fcb43c1c2c598ec779870ff
Ayush Pratap <122193870+ayushpratap27@users.noreply.github.com>
2024-10-26 21:37:26 +0530

    Added comment in App.jsx

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -6,11 +6,17 @@ import './App.css'
 function App() {
 
   const [counter, setCounter]  = useState(15)
+  // UseState changes the stage, it does not change the value but propagate the change
+  // in the UI
 
   //let counter = 15
 
   const addValue = () => {
-    //counter = counter + 1
+    //counter = counter + 1 , it is not going to update the counter in UI(actually it is
+    // increasing but not visible in UI), this is the reason to use hook as the changes should 
+    // be visible in UI also
+    // This signifies that the UI updation is controlled by React
+    
     setCounter(prevCounter => prevCounter + 1)
     setCounter(prevCounter => prevCounter + 1 )
     setCounter(prevCounter => prevCounter + 1)
